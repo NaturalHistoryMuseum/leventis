963ab7324cf8c65eed92ff2ea57d55996373cf05
benscott.co.uk <ben@benscott.co.uk>
2019-07-30 17:16:36 +0100

    Tweaks

--- setup.py
+++ setup.py
@@ -9,12 +9,9 @@ from setuptools import setup
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-# with open('HISTORY.rst') as history_file:
-#     history = history_file.read()
-
 requirements = [
-    'Click>=6.0',
-    'click-log',
+    # 'Click>=6.0',
+    # 'click-log',
     'jupyter',
     'pandas',
     'Pillow',
@@ -37,20 +34,10 @@ setup(
     author_email="ben@benscott.co.uk",
 
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+
     ],
 
-    description="Leventis OCR",
+    description="Leventis NLP",
     long_description=readme,
 
     packages=find_packages(exclude=('tests',)),
